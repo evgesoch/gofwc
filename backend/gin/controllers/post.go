521614ce5c5194b2e3375beeb15019bf1a662eee
Evgenios Sochopoulos <44399168+evgesoch@users.noreply.github.com>
2019-10-19 15:43:08 +0300

    Gin: New controllers
    - Added controllers for GET and POST

--- backend/gin/controllers/post.go
+++ backend/gin/controllers/post.go
@@ -1,32 +1,73 @@
 package controllers
 
 import (
-	_ "log"
-	_ "net/http"
+	"log"
+	"net/http"
+	"strconv"
 
+	ginModels "github.com/evgesoch/gofwc/backend/gin/models"
 	"github.com/gin-gonic/gin"
 )
-
-var c *gin.Context
 
 // Get all Posts
 func GetAllPosts() func(c *gin.Context) {
 	return func(c *gin.Context) {
+		allPosts, err := ginModels.GetAllPosts()
+		if err != nil {
+			c.String(http.StatusInternalServerError, "PostController(GetAll): Database error, can't fetch posts.")
+			log.Println(err)
+			return
+		}
 
+		c.JSON(http.StatusOK, allPosts)
 	}
 }
 
 // Get a Post
 func GetPost() func(c *gin.Context) {
 	return func(c *gin.Context) {
+		postID, err := strconv.Atoi(c.Params.ByName("postID"))
+		if err != nil {
+			c.String(http.StatusBadRequest, "PostController(Get): Can't convert URI postID param to int.")
+			log.Println(err)
+			return
+		}
 
+		post, err := ginModels.GetPostByID(postID)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "PostController(Get): Database error, can't fetch the post.")
+			log.Println(err)
+			return
+		}
+
+		if post.ID == 0 {
+			c.String(http.StatusBadRequest, "PostController(Get): This Post doesn't exist in the database.")
+			return
+		}
+
+		c.JSON(http.StatusOK, post)
 	}
 }
 
 // Create a new Post
 func CreatePost() func(c *gin.Context) {
 	return func(c *gin.Context) {
+		newPost := new(ginModels.Post)
 
+		if err := c.ShouldBindJSON(&newPost); err != nil {
+			c.String(http.StatusBadRequest, "PostController(Post): Cannot parse JSON request body.")
+			log.Println(err)
+			return
+		}
+
+		newPostID, err := ginModels.CreatePost(newPost.Text)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "PostController(Post): Database error, can't create new post.")
+			log.Println(err)
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{"postID": newPostID})
 	}
 }
 
--- backend/gin/main.go
+++ backend/gin/main.go
@@ -62,6 +62,6 @@ func setupRouter() *gin.Engine {
 func main() {
 	//r := setupRouter()
 	r := ginRouter.SetupRouter()
-	// Listen and Server in 0.0.0.0:8080
+
 	r.Run(":8080")
 }
