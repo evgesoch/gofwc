517cf9eb86b857edd6661e49b596b76334f500f9
Evgenios Sochopoulos <44399168+evgesoch@users.noreply.github.com>
2019-10-27 09:59:14 +0200

    Serve frontend from Echo

---
+++ backend/echo/frontend/js/speak4env.js
@@ -0,0 +1,4 @@
+var myObject3 = new Vue({
+    el: '#app-3',
+    data: {message: 'Hello GalZ!'}
+  });
--- backend/echo/routers/router.go
+++ backend/echo/routers/router.go
@@ -7,9 +7,14 @@ import (
 
 // Setup the routes for the echo application
 func SetupRoutes(e *echo.Echo) {
+	// Api
 	e.GET("posts", echoControllers.GetAllPosts())
 	e.GET("posts/:postID", echoControllers.GetPost())
 	e.POST("posts", echoControllers.CreatePost())
 	e.PUT("posts/:postID", echoControllers.UpdatePost())
 	e.DELETE("posts/:postID", echoControllers.DeletePost())
+
+	// Frontend
+	e.Static("/frontend", "frontend")
+	e.File("/speak4env", "frontend/index.html")
 }
