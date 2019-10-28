c7202621ea0813a7038f1bb5e3e2564dd5648937
Evgenios Sochopoulos <44399168+evgesoch@users.noreply.github.com>
2019-10-28 12:07:34 +0200

    Gin(Frontend): Added frontend code
    - Removed folder "frontend-vue"

--- backend/gin/routers/router.go
+++ backend/gin/routers/router.go
@@ -9,11 +9,16 @@ var r = gin.Default()
 
 // Main Router for gin application
 func SetupRouter() *gin.Engine {
+	// Api
 	r.GET("/posts", ginControllers.GetAllPosts())
 	r.GET("/posts/:postID", ginControllers.GetPost())
 	r.POST("/posts", ginControllers.CreatePost())
 	r.PUT("/posts/:postID", ginControllers.UpdatePost())
 	r.DELETE("/posts/:postID", ginControllers.DeletePost())
+
+	// Frontend
+	r.Static("/frontend", "./frontend")
+	r.StaticFile("/speak4env", "./frontend/index.html")
 
 	return r
 }
