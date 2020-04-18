3d96ac22030eb2239e9c7600b5200dffa860bc8d
Evgenios Sochopoulos <44399168+evgesoch@users.noreply.github.com>
2020-04-18 22:50:38 +0300

    Update defer.go, create pointers.go

--- go-code-examples/flow-control/defer/defer.go
+++ go-code-examples/flow-control/defer/defer.go
@@ -1,7 +1,57 @@
+/*
+Copyright (c) 2011 The Go Authors. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+   * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+   * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package main
 
 import "fmt"
 
+// Function defer1 shows a simple defer functionality
+func defer1() {
+	defer fmt.Println("What's up ?")
+
+	fmt.Print("Hey! ")
+}
+
+// Function defer2 demonstrates the LIFO execution of the deferred calls
+func defer2() {
+	fmt.Println("Starting to count:")
+
+	for i := 0; i < 5; i++ {
+		defer fmt.Printf("defer inside loop, i=%v\n", i)
+	}
+	defer fmt.Println("defer outside of loop")
+
+	fmt.Println("Done counting")
+}
+
 func main() {
-	fmt.Println("hi")
+	defer1()
+	defer2()
 }
