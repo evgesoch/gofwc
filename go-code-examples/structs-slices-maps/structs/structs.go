05eb70d339017a2cdd7b6a586f2b70a34ff7e0d6
Evgenios Sochopoulos <44399168+evgesoch@users.noreply.github.com>
2020-04-19 23:45:25 +0300

    Update structs.go

--- go-code-examples/structs-slices-maps/structs/structs.go
+++ go-code-examples/structs-slices-maps/structs/structs.go
@@ -1,7 +1,50 @@
+/*
+Copyright (c) 2011 The Go Authors. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+   * Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+   * Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the following disclaimer
+in the documentation and/or other materials provided with the
+distribution.
+   * Neither the name of Google Inc. nor the names of its
+contributors may be used to endorse or promote products derived from
+this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
 package main
 
 import "fmt"
 
+// Create a simple struct
+type Point struct {
+	X int
+	Y int
+}
+
 func main() {
-	fmt.Println("structs")
+	// Create and print a Point with specific X and Y
+	point1 := Point{1, 2}
+	fmt.Println(point1)
+
+	// Gain access to a Point's X field
+	point2 := Point{3, 4}
+	point2.X = 4
+	fmt.Println(point2.X)
 }
